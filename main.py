318d1cd004a834c75f9709a8c5172a1c86712375
FredrikHessner <62497929+FredrikHessner@users.noreply.github.com>
2021-12-04 21:57:47 +0100

    Added a scrollbar to the GUI

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import pandas as pd
 from tkinter import *
+from tkinter import ttk
 # Read csv file
+root = Tk()
+
+# Create A Main Frame
+main_frame = Frame(root)
+main_frame.pack(fill=BOTH, expand=1)
+# Create A Canvas
+my_canvas = Canvas(main_frame)
+my_canvas.pack(side=TOP, fill=BOTH, expand=1)
+
+# Add A Scrollbar To the Canvas
+my_scrollbar = ttk.Scrollbar(main_frame, orient=HORIZONTAL,command=my_canvas.xview)
+my_scrollbar.pack(side=BOTTOM, fill=X)
+# Configure The Canvas
+my_canvas.configure(xscrollcommand=my_scrollbar.set)
+my_canvas.bind('<Configure>',
+               lambda e: my_canvas.configure(scrollregion = my_canvas.bbox("all")))
+
+# Create Another Frame Inside the Canvas
+second_frame = Frame(my_canvas)
+
+# Add that New Frame To A Window In The Canvas
+my_canvas.create_window((0,0), window=second_frame, anchor="nw")
+
 sales = pd.read_csv(
   '/home/fredrik/PycharmProjects/GridDisplay/venv/sales_data.csv',
   parse_dates=['Date'])
@@ -18,7 +42,7 @@ data_records = pd.DataFrame(res.ravel())
 rows = []
 for i in range(len(headerArray)):
     cols = []
-    e = Entry(relief=GROOVE)
+    e = Entry(second_frame, relief=GROOVE)
     e.grid(row=0, column=i, sticky=NSEW)
     e.insert(END, headerArray[i])
     cols.append(e)
@@ -30,17 +54,13 @@ for i in range(len(data_records)):
     cols = []
 
     for j in range(len(data_records.columns)):
-
-        e = Entry(relief=GROOVE)
-
+        e = Entry(second_frame, relief=GROOVE)
         e.grid(row=i+1, column=j, sticky=NSEW)
-
         e.insert(END, data_records.iat[i,j])
         cols.append(e)
-
 
     rows.append(cols)
 
 
 
-mainloop()
+root.mainloop()
